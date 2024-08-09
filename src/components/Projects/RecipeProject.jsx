705b95f89fd4fea2e2fd6254b2d91b7bec16a4a6
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-08-09 18:54:24 +0300

    fixed project icon

--- src/components/Projects/RecipeProject.jsx
+++ src/components/Projects/RecipeProject.jsx
@@ -1,4 +1,5 @@
 import ExpressIcon from "../Icons/ExpressIcon";
+import GithubIcon from "../Icons/GithubIcon";
 import NodeIcon from "../Icons/NodeIcon";
 import ReactIcon from "../Icons/ReactIcon";
 
@@ -19,17 +20,7 @@ function RecipeProject() {
         </div>
         <div className="">
           <a className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={32}
-              height={32}
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M12 21.308L8.442 17.75l.714-.713L11.5 19.38V14.5h1v4.875l2.339-2.344l.719.719zm-5.75-5.75L2.692 12l3.552-3.552l.714.714L4.619 11.5H9.5v1H4.625l2.344 2.339zm11.5 0l-.713-.714L19.38 12.5H14.5v-1h4.875l-2.344-2.339l.719-.719L21.308 12zM11.5 9.5V4.62L9.156 6.963l-.714-.714L12 2.692l3.558 3.558l-.714.714L12.5 4.618V9.5z"
-              ></path>
-            </svg>
+            <GithubIcon />
           </a>
         </div>
       </div>
